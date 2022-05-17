9db11ad28d9a9d7419a0aefdde446650828230ca
benji uribe ramos <benji@staffdigital.pe>
2022-05-17 16:35:58 -0500

    avance mobile

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,37 @@
-let gulp          = require('gulp');
+var gulp        = require('gulp');
 let plumber       = require("gulp-plumber");
-let sass          = require('gulp-sass')(require('sass'));
-let prefix        = require("gulp-autoprefixer");
-let concat        = require('gulp-concat');
-//let sourcemaps    = require("gulp-sourcemaps");
-let notify        = require("gulp-notify");
-var serve         = require('gulp-serve');
-var refresh         = require('gulp-refresh');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass')(require('sass'));
 
 var onError = function(err){
   console.log("Se ha producido un error: ", err.message);
   this.emit("end");
 }
 
-gulp.task("sass", function(){
-  return gulp.src(['assets/sass/*.sass'])
-  .pipe(plumber({errorHandler:onError}))
-  .pipe(sass({outputStyle: 'expanded'}))
-  .pipe(prefix("last 2 versions"))
-  .pipe(gulp.dest('assets/css'))
-  //.pipe(refresh())
-  //.pipe(notify({message: "SASS tarea finalizada 💯"}))
-});
+// Compile sass into CSS & auto-inject into browsers
+function sassTask(cb) {
+   gulp.src("./assets/sass/**/*.sass")
+      .pipe(plumber({errorHandler:onError}))
+      .pipe(sass())
+      .pipe(gulp.dest("assets/css"))
+      .pipe(browserSync.stream())
+    cb();
+};
 
 
-gulp.task('serve', serve(['./']));
-gulp.task('serve-build', serve(['public', 'build']));
-gulp.task('serve-prod', serve({
-  root: ['public', 'build'],
-  port: 8081,
-  middleware: function(req, res) {
-    // custom optional middleware
-  }
-}));
+// Static Server + watching scss/html files
+function serveTask(){
 
-// ---------------------------------------
-// TAREA WATCH
-// ---------------------------------------
-gulp.task("watch", function(){
-   gulp.watch('./assets/sass/**/*.sass', gulp.series('sass'));
-});
+    browserSync.init({
+        server: "./"
+    });
 
-gulp.task('default', gulp.parallel('sass', 'serve', 'watch'));
+    gulp.watch("./assets/sass/**/*.sass", gulp.series(sassTask) );
+    gulp.watch("./**/*.html").on('change', browserSync.reload);
+
+};
+
+
+exports.sassTask = sassTask;
+exports.default = gulp.parallel(sassTask, serveTask);
+
